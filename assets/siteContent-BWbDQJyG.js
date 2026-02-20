26464d26c21cda8162c39c5794f26ae1c20118ce
Bruno Sanches Belintani <bruno.dev@amee.com.br>
2026-02-20 17:26:34 -0300

    Deploy

--- assets/siteContent-BWbDQJyG.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./index-BW0SG1OL.js";const s={list:()=>e.get("/admin/site-content"),getByType:t=>e.get(`/admin/site-content/${t}`),upsert:(t,n)=>e.put(`/admin/site-content/${t}`,n)};export{s};
